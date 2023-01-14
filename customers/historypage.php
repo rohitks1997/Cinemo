930427058d7c88551703d5c354717ba6c76c3e57
rohitks1997 <55288037+rohitks1997@users.noreply.github.com>
2023-01-14 10:39:12 +0700

    Create historypage.php

---
+++ customers/historypage.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+
+	<title>Cinemo Historypage</title>
+
+	<!-- Loading third party fonts -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+	<!-- Loading main css file -->
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="menubar.css">
+	<link rel="stylesheet" href="moviecontainer.css">
+
+	<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+</head>
+
+
+<body>
+<div id="site-content">
+        <header>
+                <a href="index.html" id="branding">
+                <img src="Cinemo Logo.JPG" alt="logo" class="logo" width="150" padding-right="30px">
+                </a> <!-- #branding -->
+                <nav>
+            <ul class="nav-area">
+                <li><a href="homepage.html">Home</a></li>
+				<li><a href="aboutpage.html">About</a></li>
+				<li><a href="allmovies.html">Movies</a></li>
+				<li><a href="historypage.html">History</a></li>
+                <li><a href="">Log Out</a></li>
+            </ul> 
+        </nav>
+        </header>
+
+	</div>
+
+
+		<!-- .main-navigation -->
+
+		<!-- Page content -->
+
+		<!-- List of Movies Section -->
+		<main class="main-content">
+				<div class="page">
+						<div class="title">
+							<h1 class="w3-center">History</h1>
+						</div>
+						<div class="row">
+							<div class="moviecontainer">
+							</div>
+							<div class="moviecontainer">
+							</div>
+							<div class="moviecontainer">
+							</div>	
+						</div> <!-- .container -->
+						<!-- Default snippet for navigation -->
+				</div>
+			</div>
+		</main>
+		<footer class="site-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2">
+					</div>
+				</div>
+				<div class="colophon">Copyright 2022 Cinemo</div>
+			</div> <!-- .container -->
+
+		</footer>
+
+		<script src="../js/jquery-1.11.1.min.js"></script>
+		<script src="../js/plugins.js"></script>
+		<script src="../js/app.js"></script>
+
+</body>
+
+</html>
