1c9a3a7128bc45967a894a6eec952bdeba517cf2
rohitks1997 <55288037+rohitks1997@users.noreply.github.com>
2023-02-17 09:28:20 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,13 @@
-<?php 
+<?php
 
-$conn= new mysqli('localhost','root','','cinemoapp')or die("Could not connect to mysql".mysqli_error($con));
+$sname= "localhost";
+$unmae= "root";
+$password = "";
+
+$db_name = "cinemoapp";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
