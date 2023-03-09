03bbe00468c166127afc8165d95e83dea9f8dc2a
rohitks1997 <55288037+rohitks1997@users.noreply.github.com>
2023-03-09 12:56:38 +0700

    Update homepage.php

--- customers/homepage.php
+++ customers/homepage.php
@@ -1,3 +1,19 @@
+<?php
+// Get the thailand current date
+date_default_timezone_set("Asia/Bangkok");
+$today = date("Y-m-d");
+//echo "Current date is " . $today;
+require "../db_connect.php";
+// Get the current showing movies query order by latest movies!
+$now_movie = $conn->query("SELECT * FROM movies WHERE release_date <= '$today' ORDER BY release_date");
+// The coming soon movies query:
+$coming_movie = $conn->query("SELECT * FROM movies WHERE release_date > '$today' ORDER BY release_date");
+
+
+//echo "number: ".$num_coming_movie;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,15 +23,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 
 	<title>Cinemo Homepage</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 	<!-- Loading third party fonts -->
 
 	<!-- Loading main css file -->
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="menubar.css">
-	<link rel="stylesheet" href="moviecontainer.css">
+	<link rel="stylesheet" href="movie.css">
 
 	<!--[if lt IE 9]>
 		<script src="js/ie-support/html5.js"></script>
@@ -28,7 +43,7 @@
 
 <div id="site-content">
         <header>
-                <a href="" id="branding">
+                <a href="index.html" id="branding">
                 <img src="Cinemo Logo.JPG" alt="logo" class="logo" width="150" padding-right="30px">
                 </a> <!-- #branding -->
                 <nav>
@@ -37,15 +52,15 @@
 				<li><a href="aboutpage.php">About</a></li>
 				<li><a href="allmovies.php">Movies</a></li>
 				<li><a href="historypage.php">History</a></li>
-                <li><a href="../signin.php">Log Out</a></li>
+                <li><a href="../signout.php">Log Out</a></li>
             </ul> 
         </nav>
         </header>
 
 	</div>
 
-<!-- Carousel -->
-<div class="container">
+
+	<div class="container">
 <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
 	<!-- Indicators/dots -->
@@ -92,44 +107,63 @@
 	</button>
   </div>
 </div>
-		
-		<!-- Page content -->
-		<main class="main-content">
+
+<main class="main-content">
 			<div class="page">
 			    <div class="w3-container w3-padding-32" id="latest movies">
 								<div class="latestmovies">
 									<h1 class="w3-center">Latest Movies</h1>
 								</div>
 							<div class="row">
-							    <div class="movie-container">
-							    </div>
-							    <div class="movie-container">
-							    </div>
-								<div class="movie-container">
-							    </div>							    
-								<div class="movie-container">
-							    </div>
+										<div class="movie-list">
+											<?php while ($row = $now_movie->fetch_assoc()) {
+												// if (mysqli_num_rows($row) > 0) {}
+											?>
+												<div class="movie">
+													<figure class="movie-poster"><a href="movie_reservation.php?id=<?php echo $row['movie_id']; ?>"><img src="<?php echo $row['cover_img'] ?>" alt="#"></a></figure>
+													<div class="text-center">
+														<div class="movie-title"><a style="text-decoration: none;" href="movie_reservation.php?id=<?php echo $row['movie_id']; ?>"><?php echo $row['title']; ?></a></div>
+													</div>
+												</div>
+											<?php
+											}
+											?>
+										</div>
 							</div>
+				</div>
+
 				<div class="w3-container w3-padding-32" id="latest movies">
-								<div class="popularmovies">
-									<h1 class="w3-center">Popular Movies</h1>
+								<div class="latestmovies">
+									<h1 class="w3-center">Upcoming Movies</h1>
 								</div>
 							<div class="row">
-									<div class="movie-container">
-									</div>
-								   <div class="movie-container">
-								   </div>
-								   <div class="movie-container">
-								   </div>							    
-								   <div class="movie-container">
-								   </div>
+										<div class="movie-list">
+											<?php while ($row = $coming_movie->fetch_assoc()) {
+												// if (mysqli_num_rows($row) > 0) {}
+											?>
+												<div class="movie">
+													<figure class="movie-poster"><a href="movie_reservation.php?id=<?php echo $row['movie_id']; ?>"><img src="<?php echo $row['cover_img'] ?>" alt="#"></a></figure>
+													<div class="text-center">
+														<div class="movie-title"><a style="text-decoration: none;" href="movie_reservation.php?id=<?php echo $row['movie_id']; ?>"><?php echo $row['title']; ?></a></div>
+													</div>
+												</div>
+											<?php
+											}
+											?>
+										</div>
 							</div>
-			</div>
-		</main>
+				</div>
+
+		
+</main>
+
+
 		<footer class="site-footer">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-2">
+						<div class="widget">
+						</div>
 					</div>
 				</div>
 				<div class="colophon">Copyright 2022 Cinemo</div>
@@ -140,6 +174,8 @@
 		</footer>
 	</div>
 	<!-- Default snippet for navigation -->
+
+
 
 	<script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/plugins.js"></script>
